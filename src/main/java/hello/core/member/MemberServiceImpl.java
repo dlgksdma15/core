d5c6ffff93efd1262848a3b3db68f7e5207d8495
hanum <dlgksdma15@naver.com>
2025-02-24 18:47:28 +0900

    2025_02_24 commit

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -11,6 +11,6 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public Member findMember(Long memberId) {
-        return memberRepository.findById(memberId); //
+        return memberRepository.findById(memberId);
     }
 }
